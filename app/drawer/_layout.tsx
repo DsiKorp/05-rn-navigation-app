fc51360ac866652369fe0a6dc0c76b6bf38890c5
Juan Delgado <dsikorp@gmail.com>
2026-05-05 16:08:46 -0500

    Tabs high ok clase 78

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -10,7 +10,7 @@ const DrawerLayout = () => {
         <Drawer
             drawerContent={CustomDrawer}
             screenOptions={{
-                headerShown: true,
+                headerShown: false,  // hide hambureger menu icon
                 overlayColor: 'rgba(0, 0, 0, 0.4)',
                 drawerActiveTintColor: 'indigo',
                 headerShadowVisible: false,
@@ -19,6 +19,16 @@ const DrawerLayout = () => {
                 },
             }}
         >
+            <Drawer.Screen
+                name="tabs" // This is the name of the page and must match the url from root
+                options={{
+                    drawerLabel: 'Tabs + Stack',
+                    title: 'Tabs + Stack',
+                    drawerIcon: ({ color, size, focused }) => (
+                        <Ionicons name="albums-outline" size={size} color={color} />
+                    )
+                }}
+            />
             <Drawer.Screen
                 name="user/index" // This is the name of the page and must match the url from root
                 options={{
